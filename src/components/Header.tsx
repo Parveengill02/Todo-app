e7fb297aadb1891d9dd559e316820f8cb614f77b
Parveengill02 <parveenkaurgill02@gmail.com>
2025-08-12 17:27:48 +0530

    adddedd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FaGithub } from "react-icons/fa";
 const Header = () => {
   return (
-    <div className='w-full bg-gray-800 flex justify-between'>
+    <div className='w-full bg-red-800 flex justify-between'>
         <div className='text-white text-xl p-2'>
            Todo App
         </div>
